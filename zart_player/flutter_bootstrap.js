5397313c30444b0fb08de411f9c05ec115dc04e5
John Evans <prujohn@gmail.com>
2025-12-11 10:53:26 -0800

    zart player update

--- zart_player/flutter_bootstrap.js
+++ zart_player/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1871820686"
+    serviceWorkerVersion: "2326219396"
   }
 });
